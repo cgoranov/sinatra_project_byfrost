d5a980bdb120e0106e9023b7070279de832fdf71
Che <cgoranov@gmail.com>
2021-05-29 09:33:26 -0500

    Database Name
    change database name to byfrost

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,7 @@ require 'bundler/setup'
 Bundler.require(:default, :development) 
 
 configure :development do
-    set :database, 'sqlite3:db/project_byfrost.db'
+    set :database, 'sqlite3:db/byfrost.db'
 end 
 
 require_all 'app'
