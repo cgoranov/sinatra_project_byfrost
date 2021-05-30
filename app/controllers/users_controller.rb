45532ec26778c0281f575f42448b745d260ef9fe
Che <cgoranov@gmail.com>
2021-05-30 07:20:41 -0500

    new flash update /signup route
    added new if/else to check if username is already intable to flash message that username is taken and requier user to try again

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,10 +15,14 @@ class UsersController < ApplicationController
             session[:user_id] = user.id
             redirect to '/budgets'
         else
-            flash[:message] = "Username and password did not meet criteria. Please try again!"
-            erb :'users/sign_up'
+            if valid_password?(params[:password]) && params[:username].length.between?(6, 10) && User.all.any? {|u| u.username == params[:username].downcase}
+                flash[:message] = "Username already taken! Please try again!"
+                erb :'users/sign_up'
+            else
+                flash[:message] = "Username and password did not meet criteria. Please try again!"
+                erb :'users/sign_up'
+            end 
         end
-
     end
 
 end
