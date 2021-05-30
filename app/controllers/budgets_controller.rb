30727bb1bd18a168acaeda9e09381788128ca095
Che <cgoranov@gmail.com>
2021-05-30 10:39:36 -0500

    Show All Budgets view build

--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -3,6 +3,7 @@ class BudgetsController < ApplicationController
 
     get '/budgets' do
         if logged_in?
+            @user = current_user
             erb :'budgets/show_all_budgets'
         else
             redirect to '/'
