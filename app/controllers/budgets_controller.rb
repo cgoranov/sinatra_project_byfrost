bbcf108e9a645947d4968651a5559de06323785c
Che <cgoranov@gmail.com>
2021-05-30 11:27:43 -0500

    post route for new Budget

--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -12,10 +12,17 @@ class BudgetsController < ApplicationController
 
     get '/budgets/new' do
         if logged_in?
-            erb :new_budget
+            erb :'budgets/new_budget'
         else
             redirect to '/login'
         end
+    end
+
+    post '/budgets' do
+        new_budget = Budget.create(name: params[:name].downcase, target: params[:target])
+        current_user.budgets << new_budget
+        current_user.save
+        redirect to '/budgets'
     end
     
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,21 @@
 
 class UsersController < ApplicationController
 
-    get '/signup' do
+    get '/user/signup' do
        if logged_in?
             user = User.find_by(id: session[:user_id])
-            redirect to "/#{user.slug}"
+            redirect to "/user/#{user.slug}"
        else
             erb :'users/sign_up'
        end
     end
 
-    post '/signup' do
+    post '/user/signup' do
         if valid_password?(params[:password]) && params[:username].length.between?(6, 10) && !User.all.any? {|u| u.username == params[:username].downcase}
             user = User.create(username: params[:username].downcase, password: params[:password])
             session[:user_id] = user.id
             flash[:messsage] = nil
-            redirect to "/#{user.slug}"
+            redirect to "/user/#{user.slug}"
         else
             if valid_password?(params[:password]) && params[:username].length.between?(6, 10) && User.all.any? {|u| u.username == params[:username].downcase}
                 flash[:message] = "Username already taken! Please try again!"
@@ -27,42 +27,42 @@ class UsersController < ApplicationController
         end
     end
 
-    get '/login' do 
+    get '/user/login' do 
         if !logged_in?
             erb :'users/log_in'
         else
             user = User.find_by(id: session[:user_id])
-            redirect to "/#{user.slug}"
+            redirect to "/user/#{user.slug}"
         end
     end
 
-    post '/login' do
+    post '/user/login' do
         user = User.find_by(username: params[:username].downcase)
         if !!user && !!user.authenticate(params[:password])
             session[:user_id] = user.id
             flash[:message] = nil
-            redirect to "/#{user.slug}"
+            redirect to "/user/#{user.slug}"
         else
             flash[:message] = "Invalid Username or Password. Please try again!"
-            redirect to '/login'
+            redirect to '/user/login'
         end
     end
 
-    get '/logout' do 
+    get '/user/logout' do 
         if logged_in?
             session.clear
             redirect to '/'
         else
-            redirect to '/login'
+            redirect to '/user/login'
         end
     end
 
-    get '/:slug' do
+    get '/user/:slug' do
         if logged_in?
             @user = User.find_by_slug(params[:slug])
             erb :'users/user_profile'
         else
-            redirect to "/login"
+            redirect to "/user/login"
         end
     end
 
