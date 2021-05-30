e27958b675ecbe05d8383f9acddc96082dc98a19
Che <cgoranov@gmail.com>
2021-05-30 15:08:18 -0500

    continued build out of Post New PO route

--- app/controllers/purchase_orders_controller.rb
+++ app/controllers/purchase_orders_controller.rb
@@ -10,10 +10,20 @@ class PurchaseOrdersController < ApplicationController
     end
 
     post '/purchase_order' do
-        binding.pry
-       if valid_number?(params[:po_number]) && valid_number?(params[:po_authorized_amout]) 
-            @po = PurchaseOrder.create()
-       end
+       if valid_number?(params[:po][:po_number]) && valid_number?(params[:po][:po_authorized_amout]) 
+            @po = PurchaseOrder.create(params[:po])
+            if !!params[:po][:vendor_id]
+                new_vendor = Vendor.create(name: params[:vendor][:name])
+                @po.vendor = new_vendor
+            end
+            if !!params[:po][:budget_id]
+                new_budget = Budget.create(name: params[:budget][:name], target: params[:budget][:target])
+                @po.budget = new_budget
+            end
+        else
+            flash[:message] = "please enter only number for PO number and Authorized Amount!"
+            redirect to '/pruchase_order/new'
+        end
     end
 
 end
