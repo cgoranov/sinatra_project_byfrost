a629845a4849f5327f8085e410a80204a5e5a31b
Che <cgoranov@gmail.com>
2021-05-30 05:08:47 -0500

    Helper Methods
    added helper methods current_user and logged_in? to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,4 +15,16 @@ class ApplicationController < Sinatra::Base
         erb :main_menu
     end
 
+    helpers do 
+
+        def current_user
+            current_user = User.find_by(id: session[:user_id])
+        end
+
+        def logged_in? 
+            !!current_user
+        end
+
+    end
+
 end
