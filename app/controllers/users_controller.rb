86a3bf9acac6ae7ba9b81252cc851eef6d9b01e9
Che <cgoranov@gmail.com>
2021-05-30 05:20:24 -0500

    User Model Methods
    added slug and find by slug methods to User Model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 
 class UsersController < ApplicationController
 
+    get '/signup' do
+       if logged_in?
+            redirect to '/:slug/budgets'
+       else
+            erb :sign_up
+       end
+    end
+    
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ActiveRecord::Base
     has_many :budgets
     has_many :purchase_orders, through: :budgets
     has_secure_password
+
+    def slug 
+        self.username.gsub("-", " ")
+    end
+
+    def self.find_by_slug(slug)
+        username= slug.gsub("-", " ")
+        User.find_by(username: username)
+    end
 end
