a87c5f9d33be3d1b86f5a951a70b207b05ce7859
Che <cgoranov@gmail.com>
2021-05-30 14:40:55 -0500

    Continue New PO Form Buildout
    adding choose vendor and choose budget drop down select items to form for new po creation

--- app/controllers/purchase_orders_controller.rb
+++ app/controllers/purchase_orders_controller.rb
@@ -3,13 +3,14 @@ class PurchaseOrdersController < ApplicationController
     
     get '/purchase_order/new' do
         if logged_in?
-            erb :new_po
+            erb :'purchase_orders/new_po'
         else
             redirect to '/user/login'
         end
     end
 
     post '/purchase_order' do
+        binding.pry
        if valid_number?(params[:po_number]) && valid_number?(params[:po_authorized_amout]) 
             @po = PurchaseOrder.create()
        end
