b39fdf71347f23b306815ce32129ce9b9e158ece
Che <cgoranov@gmail.com>
2021-06-08 20:20:39 -0500

    Purchase Order Edit Form update

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -17,7 +17,7 @@ class VendorsController < ApplicationController
         @new_vendor = Vendor.new(name: params[:name].downcase)
         if @new_vendor.valid?
             @new_vendor.save
-            redirect to '/purchase_orders/new'
+            redirect to '/vendors'
         else
             @errors = @new_vendor.errors.messages.collect {|k, v| "#{k.to_s} #{v[0]}"}
             erb :'vendors/new'
@@ -27,7 +27,19 @@ class VendorsController < ApplicationController
     get '/vendors/:id/edit' do
         redirect_if_not_loggedin
         @vendor = Vendor.find_by(id: params[:id])
-        erb :'vendors/:id/edit'
+        erb :'vendors/edit'
+    end
+
+    patch '/vendors/:id' do 
+        @vendor = Vendor.find_by(id: params[:id])
+        @edit = Vendor.new(name: params[:vendor][:name])
+        if @edit.valid?
+            @vendor.update(name: params[:vendor][:name])
+            redirect to '/vendors'
+        else
+            @errors = @edit.errors.messages.collect {|k, v| "#{k.to_s} #{v[0]}"}
+            erb :'vendors/edit'
+        end
     end
 
     delete '/vendors/:id' do
